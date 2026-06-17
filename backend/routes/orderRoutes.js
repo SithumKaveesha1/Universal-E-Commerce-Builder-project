1c5a8a0b4dbd4286f0b0ab8f41fb5fd856fa53fc
SithumKaveesha1 <168362793+SithumKaveesha1@users.noreply.github.com>
2026-06-17 22:45:27 +0530

    Add Order routes

---
+++ backend/routes/orderRoutes.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import {
+  addOrderItems,
+  getOrderById,
+  updateOrderToPaid,
+  updateOrderToDelivered,
+  getMyOrders,
+  getStoreOrders
+} from '../controllers/orderController.js';
+import { protect, admin } from '../middleware/authMiddleware.js';
+
+const router = express.Router();
+
+router.route('/')
+  .post(protect, addOrderItems);
+
+router.route('/myorders')
+  .get(protect, getMyOrders);
+
+router.route('/store')
+  .get(protect, admin, getStoreOrders);
+
+router.route('/:id')
+  .get(protect, getOrderById);
+
+router.route('/:id/pay')
+  .put(protect, updateOrderToPaid);
+
+router.route('/:id/deliver')
+  .put(protect, admin, updateOrderToDelivered);
+
+export default router;
